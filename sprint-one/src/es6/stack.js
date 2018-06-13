de4a973fc7d1f25c5226bdf493eb0557190a3846
Warren Tseng <wtseng678@gmail.com>
2018-06-13 16:16:55 -0700

    Implemented stack (es6)

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.someInstance = [];
+  	this.index = 0;
   }
 
+  push(value) {
+	for (var i = this.index; i > 0; i--) {
+		this[i] = this[i - 1];
+	}
+	this[0] = value;
+	this.index++;
+  }
+
+  pop() {
+	var first = this[0];
+	for (var i = 0; i < this.index - 1; i++) {
+		this[i] = this[i + 1];
+	}
+	if (this.index > 0) {
+		this.index--;
+	}
+	return first;
+  }
+
+  size() {
+	return this.index;
+  }
 }
