f4ec57a4c374f1e56ca08702ed26106d8acde31c
Warren Tseng <wtseng678@gmail.com>
2018-06-13 16:07:33 -0700

    Implemented queue and stack (prototypal)

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.index = 0;
+  return someInstance;
 };
 
-var queueMethods = {};
+queueMethods = {};
 
+
+queueMethods.enqueue = function(value) {
+	for (var i = this.index; i > 0; i--) {
+		this[i] = this[i - 1];
+	}
+	this[0] = value;
+	this.index++;
+};
+
+queueMethods.dequeue = function() {
+	var first = this[this.index - 1];
+	delete this[this.index - 1];
+	if (this.index > 0) {
+		this.index--;
+	}
+	return first;
+};
+
+queueMethods.size = function() {
+	return this.index;
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.index = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
+stackMethods = {};
+
+stackMethods.push = function(value) {
+	for (var i = this.index; i > 0; i--) {
+		this[i] = this[i - 1];
+	}
+	this[0] = value;
+	this.index++;
+};
+
+stackMethods.pop = function() {
+	var first = this[0];
+	for (var i = 0; i < this.index - 1; i++) {
+		this[i] = this[i + 1];
+	}
+	if (this.index > 0) {
+		this.index--;
+	}
+	return first;
+};
 
 
+stackMethods.size = function() {
+	return this.index;
+};
