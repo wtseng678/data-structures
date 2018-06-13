e398b17ba7a98909d39154110b175511fe7a8160
Warren Tseng <wtseng678@gmail.com>
2018-06-13 15:52:03 -0700

    Implemented queue and stack (functional shared)

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance.index = 0;
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
+
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+	for (var i = this.index; i > 0; i--) {
+		this[i] = this[i - 1];
+	}
+	this[0] = value;
+	this.index++;
+};
 
+stackMethods.pop = function() {
+	var first = this[0];
+	for (var i = 0; i < this.index - 1; i++) {
+		this[i] = this[i + 1];
+	}
+	if (this.index > 0) {
+		this.index--;
+	}
+	return first;
+};
+
+stackMethods.size = function() {
+	return this.index;
+};
